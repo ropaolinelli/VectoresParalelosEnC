9cd2c4f0f55c0b9ef4c1b7882ea1cb37d7ae1c03
ropaolinelli <ro.paolinelli@hotmail.com>
2018-04-19 11:27:01 -0300

    Vectores parte1

--- VectoresParalelos/main.c
+++ VectoresParalelos/main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define   TAMANIO 5
 
+void mostrarAlumno( char [] [50], int [], int [], float[], int [], int[] );
 int main()
 {
-    char nombre [50];
-    int notasUno;
-    int notasDos;
-    float promedios;
+    char nombre [TAMANIO] [50]={"Juan","Maria","Alfredo","Jose","Lili"};
+    int notasUno[TAMANIO]={};
+    int notasDos[TAMANIO]={};
+    float promedios[TAMANIO]={};
+    int legajo [TAMANIO]={};
 
-
-
+    mostrarAlumno(nombre, notasUno, notasDos, promedios, legajo, TAMANIO);
 
     return 0;
+}
+
+void mostrarAlumno(char nombre [] [50], int primeraNota [], int segundaNota [], float promedio [], int legajos [], int largo [])
+{
+    int i;
+    for(i=0; i<largo; i++)
+    {
+        printf("\n L: %d, N: %s, N1: %d, N2: %d, P: %.2f", legajos [i], nombre[i], primeraNota[i], segundaNota[i], promedio[i]);
+    }
 }
